14d2777e92fccb4360de8b725be09dc30e504c4b
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-03-02 19:32:47 +0530

    Update tagall.py

--- Zaid/modules/basic/tagall.py
+++ Zaid/modules/basic/tagall.py
@@ -96,8 +96,8 @@ async def mentionall(client: Client, message: Message):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}), "
-        if usrnum == 5:
+        usrtxt += f"[{usr.user.username}](tg://user?id={usr.mention}), "
+        if usrnum == 1:
             if args:
                 txt = f"{args}\n\n{usrtxt}"
                 await client.send_message(chat_id, txt)
